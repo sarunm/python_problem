2d9842b4fe2bb5c72db746fedfc56a55f8d8b474
sarunm <smaksnick@gmail.com>
2021-02-01 11:58:34 +0700

    add problem 0011

---
+++ problem_0011.py
@@ -0,0 +1,10 @@
+#modulo
+tmp_data = []
+
+for i in range(10):
+    input_data = int(input())
+    result = input_data % 42 
+    if result not in tmp_data:
+        tmp_data.append(result)
+
+print(len(tmp_data))
